4b5971ce799a98fd9c49bdede4b1882c1be88582
ankurchdry <ankurchdry@ANKUR-CHOUDHARYs-MacBook-Pro.local>
2014-09-28 16:50:06 -0400

    my first commit

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterNumberAgeOfLiaka: UITextField!
+    
+    @IBOutlet weak var displayLaikaAge: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertLaikaAgeToHumanYears(sender: UIButton) {
+        
+        let sizeAgeOfLiaka = Double ((enterNumberAgeOfLiaka.text as NSString).doubleValue)
+        
+        let conversionConstant = 7.5
+        
+        displayLaikaAge.hidden = false
+        
+        displayLaikaAge.text = "\(sizeAgeOfLiaka * conversionConstant )" + " is the size of Laika in Dog years"
+        
+        enterNumberAgeOfLiaka.resignFirstResponder()
+        
+        
+    }
+    
 
 }
 
